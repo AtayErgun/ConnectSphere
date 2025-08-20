bba213925e186e74281f8159a4e57354ca51319e
AtayErgun <atayergun29@gmail.com>
2025-08-20 15:53:10 +0300

    Initial commit

---
+++ src/test/java/com/ergun/connectsphere/ConnectSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ergun.connectsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
